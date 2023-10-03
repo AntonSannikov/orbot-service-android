1bc90eae78245defa1b78990b0f51ab68a3a8195
AntonSannikov <w.asas@list.ru>
2023-10-03 17:02:28 +0700

    initial

---
+++ orbotservice/src/main/java/org/torproject/android/service/OrbotHelper.java
@@ -0,0 +1,10 @@
+package org.torproject.android.service;
+
+public class OrbotHelper {
+
+    public static String applicationId = "";
+
+    public static void setApplicationId(String value) {
+        applicationId = value;
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        maven { url = uri("https://raw.githubusercontent.com/guardianproject/gpmaven/master") }
+    }
+}
+
+rootProject.name = "orbot_service_publisher"
+include(":app")
+include(":orbotservice")
+ 
